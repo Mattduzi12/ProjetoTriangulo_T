4493991a5b7329e45b505489df7669ecf6665d8d
Dudabuen0 <130669340+Dudabuen0@users.noreply.github.com>
2023-10-17 14:33:34 -0300

    - Commit secundario

--- Validador de triangulo.py
+++ Validador de triangulo.py
@@ -1,15 +1,24 @@
-#Validador de triangulo
+# Validador de triangulo
 
-#aprasentação
+# aprasentação
 print('\n\t\t\t -- Validador de Triangulo')
 
-#entrada
+# entrada
 a = int(input('Informe o lado a'))
 b = int(input('Informe o lado b'))
 c = int(input('Informe o lado c'))
 
-#processamento e saida
+# processamento e saida
 if a < (b + c) and b < (a + c) and c < (a + b):
     print(f'{a}, {b}, {c} formam um triangulo!')
+
+    # tipo do triangulo
+    if (a == b) and (b == c):
+        print('Equilatero')
+    if (a == b) or (a == c) or (b == c):
+        print('Isosceles')
+    # if (a != b) and (a != c) and (b != c):
+    else:
+        print('Escaleno')
 else:
     print(f'{a}, {b}, {c} não formam um triangulo!')
